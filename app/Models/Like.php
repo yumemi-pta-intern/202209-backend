24e40957a2b0048c3378f2be5f68dbe046dc9f2c
小西 悠斗 <54394665+85yago@users.noreply.github.com>
2022-09-14 11:58:22 +0900

    feat: Likeモデルの作成

--- app/Models/Like.php
+++ app/Models/Like.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Like extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
+    protected $casts = [
+        'created_at' => 'datetime',
+    ];
+
+    protected $primaryKey = 'id';
+
+    protected $keyType = 'string';
+
+    protected $dateFormat = 'Y-m-d H:i:s';
 }
