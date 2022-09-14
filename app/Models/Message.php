8566bd211c93d2be47e99e6c633696c5ca5dfe7f
mitsuYashi <hm20011027@gmail.com>
2022-09-14 10:44:51 +0900

    feat: message model

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Message extends Model
 {
     use HasFactory;
+    public $incrementing = false;
+    protected $primaryKey = 'uuid';
+    protected $keyType = 'string';
+    
+    protected $dateFormat = 'Y-m-d H:i:s';
+    const CREATED_AT = 'create_datetime';
+    const UPDATED_AT = NULL;
+    public $timestamps = false;
+
+    
+    
 }
