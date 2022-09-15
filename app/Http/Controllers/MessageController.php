a99d1b4e86e152d7afc7e55e9041eda88242288d
mitsuYashi <hm20011027@gmail.com>
2022-09-15 14:35:29 +0900

    feat: like_status & refact

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -5,27 +5,33 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Models\Message;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
 
 class MessageController extends Controller
 {
     public function index(Request $request)
     {
-        $messages = DB::table('messages')->get();
-
-        return response()->json($messages);
+        $messages = Message::selectRaw('uuid, message, user_uuid, like_count, EXISTS(SELECT * FROM likes WHERE user_uuid=? && message_uuid=uuid) as like_status, created_at', [Auth::id()])->orderBy('created_at', 'DESC')->get();
+        // $messages = DB::table('messages')->get();
+        // $like = DB::table("likes")->where('user_uuid', Auth::user()->id)->get();
+        return response()->json([ 'status' => Response::HTTP_OK, 'data' => $messages ]);
     }
 
     public function create(Request $request)
     {
         $message = new Message;
-        $message->create([
-            'message' => 'test message',
+        $message->fill([
+            'message' => $request->input('message'),
         ]);
+        $message->save();
+
+        return response()->json([ 'status' => Response::HTTP_OK ]);
     }
 
-    public function show(Request $request)
+    public function show(Request $request, $message_id)
     {
-        $message = DB::table('messages')->where('uuid', $request->input('uuid'));
-        return response()->json($message);
+        $message = Message::selectRaw('uuid, message, user_uuid, like_count, EXISTS(SELECT * FROM likes WHERE user_uuid=? && message_uuid=uuid) as like_status, created_at', [Auth::id()])->where('uuid', $message_id)->firstOrFail();
+        return response()->json(['status' => Response::HTTP_OK, 'data' => $message]);
     }
 }
