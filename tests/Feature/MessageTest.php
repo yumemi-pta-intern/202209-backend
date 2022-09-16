8b2a6ea3ca13a4d7069b4bdbd516e98aca93dedb
mitsuYashi <hm20011027@gmail.com>
2022-09-16 12:29:36 +0900

    feat: create message test

--- tests/Feature/MessageTest.php
+++ tests/Feature/MessageTest.php
@@ -2,12 +2,16 @@
 
 namespace Tests\Feature;
 
+use App\Models\Like;
+use App\Models\Message;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Testing\Fluent\AssertableJson;
 use Symfony\Component\HttpFoundation\Response;
 use Tests\TestCase;
+use UUID\UUID;
 
 class MessageTest extends TestCase
 {
@@ -17,59 +21,105 @@ class MessageTest extends TestCase
      * @return void
      */
     use RefreshDatabase;
+
+    private $user;    
+    protected function setUp(): void
+    {
+        // userの登録とログイン
+        parent::setUp();
+        $this->user = new User;
+        $this->user->uuid = UUID::uuid7();
+        $this->user->name = 'test user';
+        $this->user->hashed_password = Hash::make('password');
+        Auth::login($this->user);
+        $this->user->save();
+    }
+
+    // 投稿をし、タイムラインを取ってこれるか
     public function test_timeline()
     {   
-        $user = new User;
-        $user->uuid = '01833eeb-8237-7a5a-897c-58133eb0514d';
-        $user->name = 'test user';
-        $user->password = 'password';
-        Auth::login($user);
         $response = $this->post('/api/message', ['message' => 'test']);
         $response = $this->get('/api/timeline');
         $response->assertStatus(200)->assertJson(fn (AssertableJson $json) =>
             $json->where('status', Response::HTTP_OK)
                 ->has("data", 1, fn ($json) =>
-                    $json->where('user_uuid', '01833eeb-8237-7a5a-897c-58133eb0514d')
+                    $json->where('user_uuid', $this->user->uuid)
                     ->where('message', 'test')
                     ->where('like_count', 0)
-                    ->where('like_status', 0)
+                    ->where('like_status', false)
+                    ->where('name', $this->user->name)
                     ->etc()
             )
         );
-        // $response->assertStatus(200)->assertJsonStructure([
-        //     'status' => Response::HTTP_OK,
-        //     'messages' => [
-        //         'user_uuid' => '01833eeb-8237-7a5a-897c-58133eb0514d',
-        //         "*"
-        //     ]
-        // ]);
     }
 
+    // いいねをした時、タイムラインに反映されているか
+    public function test_timeline_do_like()
+    {  
+        $message = new Message;
+        $message->fill([
+            'message' => 'test2'
+        ]);
+        $message->save();
+        $message_uuid = $message['uuid'];
+
+        $like = new Like;
+        $like->fill([
+            'user_uuid' => Auth::id(),
+            'message_uuid' => $message_uuid
+        ]);
+        $like->save();
+
+        $response = $this->get('/api/timeline');
+        $response->assertStatus(200)->assertJson(fn (AssertableJson $json) =>
+            $json->where('status', Response::HTTP_OK)
+                ->has("data", 1, fn ($json) =>
+                    $json->where('user_uuid', $this->user->uuid)
+                    ->where('message', 'test2')
+                    ->where('like_count', 0)
+                    ->where('like_status', true)
+                    ->etc()
+            )
+        );
+    }
+
+    // ログインしている時に投稿ができるか
     public function test_create_login()
     {
-        $user = new User;
-        $user->uuid = '01833eeb-8237-7a5a-897c-58133eb0514d';
-        $user->name = 'test user';
-        $user->password = 'password';
-        Auth::login($user);
         $response = $this->post('/api/message', ['message' => 'test']);
         $response->assertStatus(200)->assertJson(['status' => Response::HTTP_OK]);
+        $message = \App\Models\Message::all()->first();
+        $this->assertNotNull($message);
     }
+
+    // ログインしていないときはエラーを返しているか
     public function test_create_not_login()
     {
         Auth::shouldReceive('check')->andReturn(false);
         $response = $this->post('/api/message', ['message' => 'test']);
-        $response->assertStatus(500);
+        $response->assertStatus(400);
     
     }
 
+    // showメソッドの確認
     public function test_show()
     {
-        $user = new User;
-        $user->uuid = '01833eeb-8237-7a5a-897c-58133eb0514d';
-        $user->name = 'test user';
-        $user->password = 'password';
-        Auth::login($user);
-        $response = $this->get('/api/message/');
+        $message = new Message;
+        $message->fill([
+            'message' => 'show message test'
+        ]);
+        $message->save();
+        $message_uuid = $message['uuid'];
+        $response = $this->get("/api/message/${message_uuid}");
+        $response->assertStatus(200)->assertJson(fn (AssertableJson $json) =>
+            $json->where('status', Response::HTTP_OK)
+                ->has("data", 1, fn ($json) =>
+                    $json->where('user_uuid', $this->user->uuid)
+                    ->where('message', 'show message test')
+                    ->where('like_count', 0)
+                    ->where('like_status', false)
+                    ->etc()
+            )
+        );
     }
 }
