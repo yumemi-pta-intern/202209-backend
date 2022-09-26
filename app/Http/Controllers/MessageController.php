1da1e180be7f6020486df3d641e4acc381735c9f
mitsuYashi <hm20011027@gmail.com>
2022-09-26 17:33:50 +0900

    change: LikeControllerをMessageControllerに移動

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Like;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Models\Message;
@@ -44,4 +45,33 @@ class MessageController extends Controller
         return response()->json(['status' => Response::HTTP_OK, 'data' => $message]);
     }
 
+    public function like(Request $request, $message_uuid)
+    {
+        $like_doesnt_exist = Like::where([['message_uuid', $message_uuid], ['user_uuid', Auth::id()]])->doesntExist();
+        if ($like_doesnt_exist) {
+            // like の登録
+            $like = new Like;
+            $like->fill([
+                'message_uuid' => $message_uuid,
+                'user_uuid' => Auth::id()
+            ]);
+            DB::transaction(function () use ($like, $message_uuid) {
+                $like->save();
+                Message::find($message_uuid)->increment('like_count');
+            });
+        }
+        return response()->json(['status' => Response::HTTP_OK]);
+    }
+
+    public function delete_like(Request $request, $message_uuid)
+    {
+        $like_exist = Like::where([['message_uuid', $message_uuid], ['user_uuid', Auth::id()]])->exists();
+        if ($like_exist) {
+            DB::transaction(function () use ($message_uuid) {
+                $deleted = Like::where([['message_uuid', $message_uuid], ['user_uuid', Auth::id()]])->delete();
+                Message::find($message_uuid)->decrement('like_count');
+            });
+        }
+        return response()->json([], Response::HTTP_NO_CONTENT);
+    }
 }
