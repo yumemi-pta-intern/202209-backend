c818fc0ecc49480d4c3953817daa2d2c685d7bfe
mitsuYashi <hm20011027@gmail.com>
2022-09-26 17:58:51 +0900

    feat: 整合性とる用

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -58,6 +58,7 @@ class MessageController extends Controller
             DB::transaction(function () use ($like, $message_uuid) {
                 $like->save();
                 Message::find($message_uuid)->increment('like_count');
+                $this->update_like_count($message_uuid);
             });
         }
         return response()->json(['status' => Response::HTTP_OK]);
@@ -70,8 +71,17 @@ class MessageController extends Controller
             DB::transaction(function () use ($message_uuid) {
                 $deleted = Like::where([['message_uuid', $message_uuid], ['user_uuid', Auth::id()]])->delete();
                 Message::find($message_uuid)->decrement('like_count');
+                $this->update_like_count($message_uuid);
             });
         }
         return response()->json([], Response::HTTP_NO_CONTENT);
+    }
+
+    private function update_like_count($message_uuid)
+    {
+        $like_count = Like::where('message_uuid', $message_uuid)->count();
+        if (in_array($like_count, [10, 20, 40, 80, 160, 320, 640, 1280, 2560, 5120, 1240])) {
+            Message::find($message_uuid)->update(['like_count' => $like_count]);
+        }
     }
 }
