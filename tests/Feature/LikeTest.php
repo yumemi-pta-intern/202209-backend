8d4e3d72ae3438801ce789fcffdd2cd6a15a07d3
mitsuYashi <hm20011027@gmail.com>
2022-09-26 16:57:44 +0900

    add: 他ユーザーからいいねのテスト作成

--- tests/Feature/LikeTest.php
+++ tests/Feature/LikeTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Feature;
 
+use App\Http\Controllers\LikeController;
+use App\Models\Like;
 use App\Models\Message;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -11,6 +13,7 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Testing\Fluent\AssertableJson;
 use Symfony\Component\HttpFoundation\Response;
 use Tests\TestCase;
+use UUID\UUID;
 
 class LikeTest extends TestCase
 {
@@ -26,11 +29,17 @@ class LikeTest extends TestCase
     {
         parent::setUp();
 
-        // user login
+        // user 作成
         $this->user = new User();
         $this->user->name = 'test user';
         $this->user->hashed_password = Hash::make('password');
         $this->user->save();
+
+        // user2 作成
+        $this->user2 = new User();
+        $this->user2->name = 'test user2';
+        $this->user2->hashed_password = Hash::make('password');
+        $this->user2->save();
 
         Auth::login($this->user);
     }
@@ -47,11 +56,11 @@ class LikeTest extends TestCase
         // like 登録API
         $message_uuid = $message['uuid'];
         $response = $this->post("/api/message/${message_uuid}/like");
-        $response->assertStatus(200)->assertJson(['status' => Response::HTTP_OK]);
+        $response->assertStatus(Response::HTTP_OK)->assertJson(['status' => Response::HTTP_OK]);
 
         // message のlike_countが正しく更新されているか
         $message_response = $this->get("/api/message/${message_uuid}");
-        $message_response->assertStatus(200)->assertJson(fn (AssertableJson $json) =>
+        $message_response->assertStatus(Response::HTTP_OK)->assertJson(fn (AssertableJson $json) =>
             $json->where('status', Response::HTTP_OK)
             ->has("data", 1, fn ($json) =>
                 $json->where('user_uuid', $this->user->uuid)
@@ -61,7 +70,10 @@ class LikeTest extends TestCase
                 ->where('name', $this->user->name)
                 ->etc()
             )
-        );   
+        );
+
+        $like = Like::where([['user_uuid', Auth::id()], ["message_uuid", $message_uuid]])->first();
+        $this->assertNotNull($like);
     }
 
     public function test_like_delete()
@@ -76,16 +88,16 @@ class LikeTest extends TestCase
         // like 登録API
         $message_uuid = $message['uuid'];
         $response = $this->post("/api/message/${message_uuid}/like");
-        $response->assertStatus(200)->assertJson(['status' => Response::HTTP_OK]);
+        $response->assertStatus(Response::HTTP_OK)->assertJson(['status' => Response::HTTP_OK]);
 
         // like 削除API
         $message_uuid = $message['uuid'];
         $response = $this->delete("/api/message/${message_uuid}/like");
-        $response->assertStatus(200)->assertJson(['status' => Response::HTTP_OK]);
+        $response->assertStatus(Response::HTTP_NO_CONTENT);
 
-        // message のlike_countが正しく更新されているか
+        // message のlike_count の確認
         $message_response = $this->get("/api/message/${message_uuid}");
-        $message_response->assertStatus(200)->assertJson(fn (AssertableJson $json) =>
+        $message_response->assertStatus(Response::HTTP_OK)->assertJson(fn (AssertableJson $json) =>
             $json->where('status', Response::HTTP_OK)
             ->has("data", 1, fn ($json) =>
                 $json->where('user_uuid', $this->user->uuid)
@@ -98,7 +110,7 @@ class LikeTest extends TestCase
         );   
     }
 
-    public function test_like_create_double()
+    public function test_like_create_duplicate()
     {
         // message 作成
         $message = new Message;
@@ -110,14 +122,14 @@ class LikeTest extends TestCase
         // like 登録API
         $message_uuid = $message['uuid'];
         $response = $this->post("/api/message/${message_uuid}/like");
-        $response->assertStatus(200)->assertJson(['status' => Response::HTTP_OK]);
+        $response->assertStatus(Response::HTTP_OK);
 
         $message_uuid = $message['uuid'];
         $response = $this->post("/api/message/${message_uuid}/like");
-        $response->assertStatus(409);
+        $response->assertStatus(Response::HTTP_OK);
     }
 
-    public function test_like_null_delete()
+    public function test_like_delete_null()
     {
         // message 作成
         $message = new Message;
@@ -129,7 +141,44 @@ class LikeTest extends TestCase
         // like 削除API
         $message_uuid = $message['uuid'];
         $response = $this->delete("/api/message/${message_uuid}/like");
-        $response->assertStatus(409);
+        $response->assertStatus(Response::HTTP_NO_CONTENT);
+    }
+
+    public function test_like_create_two_user() 
+    {
+        // message 作成
+        $message = new Message;
+        $message->fill([
+            'message' => 'test message',
+        ]);
+        $message->save();
+
+        // like 登録API
+        $message_uuid = $message['uuid'];
+        $response = $this->post("/api/message/${message_uuid}/like");
+        $response->assertStatus(Response::HTTP_OK)->assertJson(['status' => Response::HTTP_OK]);
+
+        // user 変更
+        Auth::login($this->user2);
+
+        // like 登録API
+        $message_uuid = $message['uuid'];
+        $response = $this->post("/api/message/${message_uuid}/like");
+        $response->assertStatus(Response::HTTP_OK)->assertJson(['status' => Response::HTTP_OK]);
+
+        // message のlike_count の確認
+        $message_response = $this->get("/api/message/${message_uuid}");
+        $message_response->assertStatus(Response::HTTP_OK)->assertJson(fn (AssertableJson $json) =>
+            $json->where('status', Response::HTTP_OK)
+            ->has("data", 1, fn ($json) =>
+                $json->where('user_uuid', $this->user->uuid)
+                ->where('message', 'test message')
+                ->where('like_count', 2)
+                ->where('like_status', true)
+                ->where('name', $this->user->name)
+                ->etc()
+            )
+        );
     }
 
 }
