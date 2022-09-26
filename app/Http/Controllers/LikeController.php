0b790d0ecdacc2203bc5ab3cbd47b5e067947e01
mitsuYashi <hm20011027@gmail.com>
2022-09-26 16:56:21 +0900

    feat: transaction 処理の追加

--- app/Http/Controllers/LikeController.php
+++ app/Http/Controllers/LikeController.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 use App\Http\Controllers\MessageController;
 use App\Models\Message;
+use Illuminate\Support\Facades\DB;
 
 class LikeController extends Controller
 {
@@ -22,22 +23,23 @@ class LikeController extends Controller
                 'message_uuid' => $message_uuid,
                 'user_uuid' => Auth::id()
             ]);
-            $like->save();
-            Message::find($message_uuid)->increment('like_count');
-            return response()->json(['status' => Response::HTTP_OK]);
-        } else {
-            return response()->json(['status' => Response::HTTP_CONFLICT], Response::HTTP_CONFLICT);
+            DB::transaction(function () use ($like, $message_uuid) {
+                $like->save();
+                Message::find($message_uuid)->increment('like_count');
+            });
         }
+        return response()->json(['status' => Response::HTTP_OK]);
     }
+
     public function delete(Request $request, $message_uuid)
     {
         $like_exist = Like::where([['message_uuid', $message_uuid], ['user_uuid', Auth::id()]])->exists();
         if ($like_exist) {
-            $deleted = Like::where([['message_uuid', $message_uuid], ['user_uuid', Auth::id()]])->delete();
-            Message::find($message_uuid)->decrement('like_count');
-            return response()->json(['status' => Response::HTTP_OK]);
-        } else {
-            return response()->json(['status' => Response::HTTP_CONFLICT], Response::HTTP_CONFLICT);
+            DB::transaction(function () use ($message_uuid) {
+                $deleted = Like::where([['message_uuid', $message_uuid], ['user_uuid', Auth::id()]])->delete();
+                Message::find($message_uuid)->decrement('like_count');
+            });
         }
+        return response()->json([], Response::HTTP_NO_CONTENT);
     }
 }
