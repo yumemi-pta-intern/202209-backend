a20fe110662d9a5ddf96ed53b078196423f36b70
85yago <54394665+85yago@users.noreply.github.com>
2022-09-28 13:56:01 +0900

    fix: ミドルウェアをまとめて書く様に修正

--- routes/api.php
+++ routes/api.php
@@ -22,11 +22,14 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('/signup', [UserController::class, 'signup']);
 Route::post('/login', [UserController::class, 'login']);
-Route::post('/logout', [UserController::class, 'logout'])->middleware('auth');
-Route::get('/user/me/', [UserController::class, 'getMe'])->middleware('auth');
-Route::get('/user/{user_id}', [UserController::class, 'getProfile'])->middleware('auth');
-Route::put('/user/profile', [UserController::class, 'updateProfile'])->middleware('auth');
-Route::put('/user/password', [UserController::class, 'updatePassword'])->middleware('auth');
+
+Route::middleware(['auth'])->group(function () {
+    Route::post('/logout', [UserController::class, 'logout']);
+    Route::get('/user/me/', [UserController::class, 'getMe']);
+    Route::get('/user/{user_id}', [UserController::class, 'getProfile']);
+    Route::put('/user/profile', [UserController::class, 'updateProfile']);
+    Route::put('/user/password', [UserController::class, 'updatePassword']);
+});
 
 Route::get('/timeline', [MessageController::class, 'index']);
 Route::post('/message', [MessageController::class, 'create']);
